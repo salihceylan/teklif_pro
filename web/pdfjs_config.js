1ba71de94a9a05edbef09fdc0c6c271791699e94
salihceylan <salihceylan@gmail.com>
2026-03-24 16:56:10 +0300

    Fix web PDF preview bootstrap order

--- web/pdfjs_config.js
+++ web/pdfjs_config.js
@@ -1,1 +1,7 @@
 window.dartPdfJsBaseUrl = 'assets/js/pdf/3.2.146/';
+window.dartPdfJsVersion = '3.2.146';
+
+if (window.pdfjsLib && window.pdfjsLib.GlobalWorkerOptions) {
+  window.pdfjsLib.GlobalWorkerOptions.workerSrc =
+    `${window.dartPdfJsBaseUrl}pdf.worker.min.js`;
+}
